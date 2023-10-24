37db11e0f3bd5f730ee89a83097b652d72592652
K1netis <wtvistet@gmail.com>
2023-10-24 14:27:17 +0300

    update: main and hero add: hero.png

--- main.py
+++ main.py
@@ -1,17 +1,28 @@
 import pygame
 import sys
+from hero import Hero
 
 def start_game():
     '''Основная функция для описания игры'''
     pygame.init()
-    screen = pygame.display.set_mode((800, 600))
+    screen = pygame.display.set_mode((800, 900))
     pygame.display.set_caption("Самая лучшая игра")
+
+    hero = Hero(screen)
 
     flag = True
     while flag:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
-
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_d:
+                    hero.rect.centerx += 10
+                if event.key == pygame.K_a:
+                    hero.rect.centerx -= 10
+        
+        pygame.display.flip()
+        screen.fill(0)
+        hero.output_hero()
 
 start_game()
