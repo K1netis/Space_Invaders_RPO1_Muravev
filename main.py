f3108832bfce65ef59e09db6f87e1832a33e53ce
K1netis <wtvistet@gmail.com>
2023-10-30 17:46:20 +0300

    update: main.py

--- main.py
+++ main.py
@@ -5,22 +5,31 @@ from hero import Hero
 def start_game():
     '''Основная функция для описания игры'''
     pygame.init()
-    screen = pygame.display.set_mode((800, 900))
+    screen = pygame.display.set_mode((1200, 900))
     pygame.display.set_caption("Самая лучшая игра")
 
     hero = Hero(screen)
 
     flag = True
+    move_left = False
+    move_right = False
     while flag:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_d:
-                    hero.rect.centerx += 10
+                    move_right = True
                 if event.key == pygame.K_a:
-                    hero.rect.centerx -= 10
+                    move_left = True
+            if event.type == pygame.KEYUP:
+                move_right = False
+                move_left = False
         
+        if move_right == True and hero.rect.right < 1200:
+            hero.rect.centerx += 1
+        if move_left == True and hero.rect.left > 0:
+            hero.rect.centerx -= 1
         pygame.display.flip()
         screen.fill(0)
         hero.output_hero()
